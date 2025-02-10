38f43cad186b46f0ab9fb46c01489be0b8009f6d
Aman Rai <raiaman987@gmail.com>
2025-02-10 13:12:58 +0530

    update

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+import cors from "cors";
 import express, { urlencoded } from "express";
 import EmployeeRouter from "./routes/fe_assg.js";
-
 const app = express();
 
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
+app.use(cors());
 app.use("/api",EmployeeRouter)
 export default app;
